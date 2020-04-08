4f5b71c7c0853f59d0cac0f68ff6e9c58ff56b62
Владимир <www.Enerdje@mail.ru>
2020-04-09 05:33:09 +0600

    Добавьте файлы проекта.

---
+++ QuadEq.Tests/Program.Tests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+
+namespace QuadEq.Tests
+{
+    public class Program_Tests
+    {
+        [TestCase(1, 0, 1)]
+        [TestCase(2, 5, -3.5)]
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 4, 1)]
+        [TestCase(0, 4, 1)] //Первый коэф. не может быть нулём.
+        public void Solution_Test(double a, double b, double c)
+        {
+            bool result = Program.Solution(a, b, c);
+            Assert.IsTrue(result);
+        }
+
+        #region Описание
+        //Тест на корректный вход данных
+        //Можно сделать перегрузку метода без out параметров. 
+        //Не выгодно перегружать метод ради теста и в реалиях это не всегда возможно.
+        #endregion
+        [Test] 
+        public void CorrectWrite_Test()
+        {
+            string[] Test = new string[] {"1 2 5", "10 4 2", "11 1 5", "1 1 1", "2 6 4"};
+            foreach (string i in Test)
+            {
+                bool result = Program.CorrectWrite(i, out _, out _, out _);
+                Assert.IsTrue(result, $"Ошибка в строке: ({i})");
+            }
+        }
+    }
+}
---
+++ QuadEq/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace QuadEq
+{
+    public class Program
+    {
+        static void Main()
+        {
+            #region Чтение из файла
+            string s = "============================================";
+            string path = Directory.GetCurrentDirectory() + @"\Tests.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine($"Вид квадратного уравнения: ax^2 + bx + c = 0\n{s}");
+                using StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
+                string inputLine;
+                while ((inputLine = sr.ReadLine()) != null)
+                {
+                    if (CorrectWrite(inputLine, out double a, out double b, out double c)) 
+                        Solution(a, b, c);
+                    else Console.WriteLine($"Некорректная строка: {inputLine}\n{s}");
+                }
+            }
+            else { Console.WriteLine("Создайте в папке с программой файл Tests.txt\nВведите в файл значения в формате \"4 7,4 2\"."); }
+            #endregion
+            #region Ручной ввод
+            while (true)
+            {
+                Console.Write("Введите коэффициенты в формарте \"4 5,5 7\":");
+                if (CorrectWrite(Console.ReadLine(), out double a, out double b, out double c))
+                {
+                    Solution(a, b, c);
+                    Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }    
+            }
+            #endregion
+        }
+        public static bool CorrectWrite(string input, out double a, out double b, out double c)
+        {
+            string[] x = input.Split();
+            if (x.Length != 3 || !double.TryParse(x[0], out _) || !double.TryParse(x[1], out _)
+                              || !double.TryParse(x[2], out _) || double.Parse(x[0]) == 0)
+            {
+                a = 0; b = 0; c = 0;
+                return false;
+            }
+            else
+            {
+                a = double.Parse(x[0]); b = double.Parse(x[1]); c = double.Parse(x[2]);
+                return true;
+            }
+        }
+        public static bool Solution(double a, double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            Console.WriteLine($"Считано на входе: ({a}) ({b}) ({c})");
+            Console.WriteLine($"Составлено уравнение: {a}x^2 + {b}x + {c} = 0");
+            Console.WriteLine($"Дискриминант равен: {D}");
+            switch (D)
+            {
+                case double d when d > 0:
+                    Console.WriteLine($"Первый корень равен: {((-b - Math.Sqrt(D)) / (2 * a))}\n" +
+                                      $"Второй корень равен: {((-b + Math.Sqrt(D)) / (2 * a))}");
+                    break;
+                case double d when d == 0:
+                    Console.WriteLine($"Корень равен: {(-b / (2 * a))}");
+                    break;
+                case double d when d < 0:
+                    Console.WriteLine("Уравнение не имеет действительных решений.");
+                    break;
+            }
+            Console.WriteLine("============================================");
+            return (a == 0) ? false : true;
+        }
+    }
+}
