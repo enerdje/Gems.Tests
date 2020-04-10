34240e9d4b53915d115c6d2f6dd1f5b8070b3f26
Владимир <www.Enerdje@mail.ru>
2020-04-10 06:19:31 +0600

    update after code review

--- QuadEq.Tests/Program.Tests.cs
+++ QuadEq.Tests/Program.Tests.cs
@@ -1,41 +1,30 @@
 using NUnit.Framework;
+using System;
 
 namespace QuadEq.Tests
 {
     public class Program_Tests
     {
-        [TestCase(1, 0, 1)]
-        [TestCase(2, 5, -3.5)]
-        [TestCase(1, 1, 1)]
-        [TestCase(1, 4, 1)]
-        [TestCase(0, 4, 1)] //Первый коэф. не может быть нулём.
-        public void Solution_Test(double a, double b, double c)
+        [TestCase("1 0 1")]
+        [TestCase("2 5 -3,5")]
+        [TestCase("1 1 1")]
+        [TestCase("1 4 1")]
+        [TestCase("0 4 1")]
+        public void Solution_Test(string input)
         {
-            bool result = Program.Solution(a, b, c);
+            bool result = Program.Solution(input);
             Assert.IsTrue(result);
         }
 
-        [TestCase(1, 0, 1, ExpectedResult = "Уравнение не имеет действительных решений.")]
-        [TestCase(2, 5, -3.5d, ExpectedResult = "-3,0700274723201293 0,5700274723201295")]
-        [TestCase(1, 1, 1, ExpectedResult = "Уравнение не имеет действительных решений.")]
-        [TestCase(1, 4, 1, ExpectedResult = "-3,732050807568877 -0,2679491924311228")]
-        [TestCase(0, 4, 1, ExpectedResult = "Ошибка. Первый коэф. не может быть 0.")]
-        public string Solution_Test_2(double a, double b, double c)
+        [TestCase("1 0 1", 0, ExpectedResult = "Уравнение не имеет действительных решений.")]
+        [TestCase("2 5 -3,5", 0, ExpectedResult = "-3,0700274723201293 0,5700274723201295")]
+        [TestCase("1 1 1", 0, ExpectedResult = "Уравнение не имеет действительных решений.")]
+        [TestCase("1 4 1", 0, ExpectedResult = "-3,732050807568877 -0,2679491924311228")]
+        [TestCase("0 4 1", 0, ExpectedResult = "Ошибка. Первый коэф. не может быть 0.")]
+        public string Solution_Test_Values(string values, int _)
         {
-            string result = Program.Solution(a, b, c, 0);
+            string result = Program.Solution(values, 0);
             return result;
-        }
-
-        //Тест на корректный вход данных
-        [Test]
-        public void CorrectWrite_Test()
-        {
-            string[] Test = new string[] { "1 2 5", "10 4 2", "11 1 5", "1 1 1", "2 6 4" };
-            foreach (string i in Test)
-            {
-                bool result = Program.CorrectWrite(i, out _, out _, out _);
-                Assert.IsTrue(result, $"Ошибка в строке: ({i})");
-            }
         }
     }
 }
--- QuadEq/Program.cs
+++ QuadEq/Program.cs
@@ -5,95 +5,113 @@ namespace QuadEq
 {
     public class Program
     {
+        public static readonly string s = "================================================================\n";
         static void Main()
         {
-            #region Чтение из файла
-            string s = "============================================";
             string path = Directory.GetCurrentDirectory() + @"\Tests.txt";
-            if (File.Exists(path))
-            {
-                Console.WriteLine($"Вид квадратного уравнения: ax^2 + bx + c = 0\n{s}");
-                using StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
-                string inputLine;
-                while ((inputLine = sr.ReadLine()) != null)
-                {
-                    if (CorrectWrite(inputLine, out double a, out double b, out double c))
-                        Solution(a, b, c);
-                    else Console.WriteLine($"Некорректная строка: {inputLine}\n{s}");
-                }
-            }
-            else { Console.WriteLine("Создайте в папке с программой файл Tests.txt\nВведите в файл значения в формате \"4 7,4 2\"."); }
-            #endregion
-            #region Ручной ввод
+
             while (true)
             {
-                Console.Write("Введите коэффициенты в формарте \"4 5,5 7\":");
-                if (CorrectWrite(Console.ReadLine(), out double a, out double b, out double c))
+                Console.WriteLine($"{s}Вид квадратного уравнения: ax^2 + bx + c = 0");
+                Console.Write($"Выберите режим ввода (1-чтение из файла; 2-ручной ввод):");
+                if (int.TryParse(Console.ReadLine(), out int value))
                 {
-                    Solution(a, b, c);
-                    Console.WriteLine("Для продолжения нажмите любую клавишу.");
-                    Console.ReadKey();
-                    Console.Clear();
+                    switch (value)
+                    {
+                        #region Чтение из файла
+                        case 1:
+                            if (File.Exists(path))
+                            {
+                                using StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
+                                string inputLine;
+                                while ((inputLine = sr.ReadLine()) != null)
+                                {
+                                    Solution(inputLine);
+                                }
+                            }
+                            else { Console.WriteLine("В папке со сборкой не обнаружен файл Tests.txt."); }
+                            break;
+                        #endregion
+                        #region Ручной ввод
+                        case 2:
+                            Console.Write($"{s}Введите коэффициенты в формарте \"A B C\":");
+                            Solution(Console.ReadLine());
+                            Console.WriteLine($"{s}Для продолжения нажмите любую клавишу.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        #endregion
+                        default:
+                            Console.WriteLine("Ошибка! Доступно только две режима (1 или 2).");
+                            break;
+                    }
                 }
+                else Console.WriteLine("Ошибка! Вы ввели не число.");
             }
-            #endregion
-        }
-        public static bool CorrectWrite(string input, out double a, out double b, out double c)
-        {
-            string[] x = input.Split();
-            if (x.Length != 3 || !double.TryParse(x[0], out _) || !double.TryParse(x[1], out _)
-                              || !double.TryParse(x[2], out _) || double.Parse(x[0]) == 0)
-            {
-                a = 0; b = 0; c = 0;
-                return false;
-            }
-            else
-            {
-                a = double.Parse(x[0]); b = double.Parse(x[1]); c = double.Parse(x[2]);
-                return true;
-            }
-        }
-        public static bool Solution(double a, double b, double c)
-        {
-            double D = b * b - 4 * a * c;
-            Console.WriteLine($"Считано на входе: ({a}) ({b}) ({c})");
-            Console.WriteLine($"Составлено уравнение: {a}x^2 + {b}x + {c} = 0");
-            Console.WriteLine($"Дискриминант равен: {D}");
-            switch (D)
-            {
-                case double d when d > 0:
-                    Console.WriteLine($"Первый корень равен: {((-b - Math.Sqrt(D)) / (2 * a))}\n" +
-                                      $"Второй корень равен: {((-b + Math.Sqrt(D)) / (2 * a))}");
-                    break;
-                case double d when d == 0:
-                    Console.WriteLine($"Корень равен: {(-b / (2 * a))}");
-                    break;
-                case double d when d < 0:
-                    Console.WriteLine("Уравнение не имеет действительных решений.");
-                    break;
-            }
-            Console.WriteLine("============================================");
-            return (a == 0) ? false : true;
         }
 
         //Перегруженый метод для сверки значений.
-        public static string Solution(double a, double b, double c, int _)
+        public static string Solution(string input, int _)
         {
-            if (a != 0)
+            InputValidation(input, out double a, out double b, out double c);
+            double D = b * b - 4 * a * c;
+            return D switch
             {
+                double d when d > 0 => $"{((-b - Math.Sqrt(D)) / (2 * a))} {((-b + Math.Sqrt(D)) / (2 * a))}",
+                double d when d == 0 => $"{(-b / (2 * a))}",
+                double d when d < 0 => "Уравнение не имеет действительных решений.",
+                _ => "",
+            };
+        }
+
+        public static bool Solution(string input)
+        {
+            try
+            {
+                InputValidation(input, out double a, out double b, out double c);
                 double D = b * b - 4 * a * c;
+                Console.WriteLine($"{s}Считано на входе: ({a}) ({b}) ({c})");
+                Console.WriteLine($"Составлено уравнение: {a}x^2 + {b}x + {c} = 0");
+                Console.WriteLine($"Дискриминант равен: {D}");
                 switch (D)
                 {
                     case double d when d > 0:
-                        return $"{((-b - Math.Sqrt(D)) / (2 * a))} {((-b + Math.Sqrt(D)) / (2 * a))}";
+                        Console.WriteLine($"Первый корень равен: {((-b - Math.Sqrt(D)) / (2 * a))}\n" +
+                                          $"Второй корень равен: {((-b + Math.Sqrt(D)) / (2 * a))}");
+                        break;
                     case double d when d == 0:
-                        return $"{(-b / (2 * a))}";
+                        Console.WriteLine($"Корень равен: {(-b / (2 * a))}");
+                        break;
                     case double d when d < 0:
-                        return "Уравнение не имеет действительных решений.";
+                        Console.WriteLine("Уравнение не имеет действительных решений.");
+                        break;
                 }
-                return "";
+                return true;
             }
-            else { return "Ошибка. Первый коэф. не может быть 0."; }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{s}Ошибка: {e.Message}");
+                return false;
+            }
+        }
+
+        public static bool InputValidation(string input, out double a, out double b, out double c)
+        {
+            string[] values = input.Split();
+            if (!double.TryParse(values[0], out _) || !double.TryParse(values[1], out _) || !double.TryParse(values[2], out _))
+            {
+                throw new Exception($"Входной/ые параметры  не являются числом! Строка: {input}");
+            }
+            if (values.Length != 3)
+            {
+                throw new Exception($"Входных параметров должно быть 3! Строка: {input}");
+            }
+            if (double.Parse(values[0]) == 0)
+            {
+                throw new Exception($"Входной коэффициент \"A\" не может быть 0! Строка: {input}");
+            }
+            a = double.Parse(values[0]); b = double.Parse(values[1]); c = double.Parse(values[2]);
+            return true;
         }
     }
 }
